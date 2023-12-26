deafd08b0fa69e7ab1bc12ab21e86f7bee9401db
ShowmikDebnath <67783022+ShowmikDebnath@users.noreply.github.com>
2023-12-26 16:18:26 +0600

    Report - Sanction Detail Information done

---
+++ lib/Screens/DesktopScreen/DesktopDashBoard/Reports/SanctionDetailInformation.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:prottashasomit/Widget/Appbar.dart';
+import 'package:prottashasomit/Widget/Appbool.dart';
+import 'package:prottashasomit/Widget/NavBool.dart';
+import 'package:prottashasomit/Widget/NoDataFound.dart';
+import '../../../../Constants/Constants.dart';
+import '../../../../Widget/NavbarScreen.dart';
+import '../../../../Widget/ReportsWidgets/Ledger.dart';
+import '../../../../Widget/ReportsWidgets/SanctionDetail.dart';
+import '../../../../Widget/ReportsWidgets/TransactionList.dart';
+import '../../../../Widget/SamiteeSelection.dart';
+
+
+class SanctionDetailInformation extends StatefulWidget {
+  Navbool navbool;
+  Appbool appbool;
+
+  SanctionDetailInformation({required this.appbool, required this.navbool});
+
+  @override
+  State<SanctionDetailInformation> createState() => _SanctionDetailInformationState();
+}
+
+class _SanctionDetailInformationState extends State<SanctionDetailInformation> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: Appbar(
+        navbool: widget.appbool,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            NavbarScreen(appbool: widget.appbool, navbool: widget.navbool,),
+
+            SizedBox(
+              height: 50,
+            ),
+
+
+            // SANCTION DETAILS INFORMATION
+            SanctionDetail(),
+
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
