e7031666a47688320d8e09c6464992e762895fd7
MrRoy121 <nilashishroyjoy@gmail.com>
2024-01-20 00:32:04 +0600

    Updated With Authentication

--- lib/Widget/Appbar.dart
+++ lib/Widget/Appbar.dart
@@ -4,6 +4,7 @@ import 'package:prottashasomit/Widget/AppBarItem.dart';
 import 'package:get/get.dart';
 
 import '../Constants/Constants.dart';
+import '../helpers/auth_service.dart';
 import '../route.dart';
 
 class Appbar extends AppBar {
@@ -85,7 +86,108 @@ class _NavbarState extends State<Appbar> {
                                 widget.navbool.makeallfalse();
                                 widget.navbool.admin = true;
                               });
-                              Navigator.of(context).pushNamed('admin_titas');
+
+                              Get.dialog(
+                                  barrierColor: Colors.transparent,
+                                  barrierDismissible: true,
+                                  Dialog(
+                                    insetPadding: EdgeInsets.only(
+                                        right: 10, top: 25),
+                                    backgroundColor: Colors.white,
+                                    elevation: 20,
+                                    alignment: Alignment.topRight,
+                                    child: Container(
+                                      width: 300,
+                                      height: 500,
+                                      padding: EdgeInsets.all(20),
+                                      child: Column(
+                                        children: [
+                                          const SizedBox(
+                                            height: 15,
+                                          ),
+                                          Text(
+                                            "Admin",
+                                            style: TextStyle(
+                                              color: Colors.black87,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                          Text(
+                                            "ID: usr?.id",
+                                            style: TextStyle(
+                                              color: Colors.black87,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                          const Expanded(
+                                              child: SizedBox(
+                                            height: 0,
+                                          )),
+                                          InkWell(
+                                            onTap: () {
+                                            },
+                                            child: Container(
+                                              padding: const EdgeInsets.only(
+                                                  left: 20, right: 20, top: 5),
+                                              margin: const EdgeInsets.only(
+                                                  bottom: 10),
+                                              child: Row(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  const Icon(Icons.person,
+                                                      color: Colors.black87),
+                                                  const SizedBox(
+                                                    width: 15,
+                                                  ),
+                                              Text(
+                                                "My Profile",
+                                                style: TextStyle(
+                                                color: Colors.black87,
+                                                fontSize: 16,
+                                              ),
+                                            ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          InkWell(
+                                            onTap: () {
+                                              AuthService.to.Logout();
+                                              Get.offAllNamed(
+                                                  authenticationPageRoute);
+                                            },
+                                            child: Container(
+                                              padding: const EdgeInsets.only(
+                                                  left: 20, right: 20, top: 5),
+                                              margin: const EdgeInsets.only(
+                                                  bottom: 10),
+                                              child: Row(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  const Icon(Icons.logout,
+                                                      color: Colors.black87),
+                                                  const SizedBox(
+                                                    width: 15,
+                                                  ),Text(
+                                                    "Logout",
+                                                    style: TextStyle(
+                                                      color: Colors.black87,
+                                                      fontSize: 16,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ));
                             },
                             child: AppbarItem(
                                 flag: widget.navbool.admin,
@@ -98,7 +200,11 @@ class _NavbarState extends State<Appbar> {
                         Get.dialog(
                             barrierColor: Colors.transparent,
                             barrierDismissible: true,
-                            Dialog(alignment: Alignment.topRight, insetPadding: const EdgeInsets.only(top: 45,),
+                            Dialog(
+                              alignment: Alignment.topRight,
+                              insetPadding: const EdgeInsets.only(
+                                top: 45,
+                              ),
                               child: Container(
                                 color: AppColor,
                                 // height: 800,
@@ -106,9 +212,7 @@ class _NavbarState extends State<Appbar> {
                                 child: Column(
                                   children: [
                                     InkWell(
-                                      onHover: (val) {
-
-                                      },
+                                      onHover: (val) {},
                                       onTap: () {
                                         // Get.to(SamiteeRegistration(
                                         //   appbool: widget.appbool,
@@ -134,13 +238,12 @@ class _NavbarState extends State<Appbar> {
                                       ),
                                     ),
                                     InkWell(
-
                                       onTap: () {
                                         setState(() {
                                           widget.navbool.makeallfalse();
                                           widget.navbool.home = true;
                                         });
-                                       Get.toNamed(homePageRoute);
+                                        Get.toNamed(homePageRoute);
                                       },
                                       child: Container(
                                           padding: EdgeInsets.only(
@@ -160,9 +263,7 @@ class _NavbarState extends State<Appbar> {
                                           )),
                                     ),
                                     InkWell(
-                                      onHover: (val) {
-
-                                      },
+                                      onHover: (val) {},
                                       onTap: () {
                                         setState(() {
                                           widget.navbool.makeallfalse();
@@ -184,11 +285,8 @@ class _NavbarState extends State<Appbar> {
                                             ),
                                           )),
                                     ),
-
                                     InkWell(
-                                      onHover: (val) {
-
-                                      },
+                                      onHover: (val) {},
                                       onTap: () {
                                         // Get.to(EditMembers(
                                         //   appbool: widget.appbool,
@@ -209,17 +307,15 @@ class _NavbarState extends State<Appbar> {
                                             ),
                                           )),
                                     ),
-
                                     InkWell(
-                                      onHover: (val) {
-
-                                      },
+                                      onHover: (val) {},
                                       onTap: () {
                                         setState(() {
                                           widget.navbool.makeallfalse();
                                           widget.navbool.admin = true;
                                         });
-                                        Navigator.of(context).pushNamed('admin_titas');
+                                        Navigator.of(context)
+                                            .pushNamed('admin_titas');
                                       },
                                       child: Container(
                                           padding: EdgeInsets.only(
--- lib/route.dart
+++ lib/route.dart
@@ -1,21 +1,18 @@
-
-
-const   homePageRoute = "/home";
-const   dashboardPageRoute = "/dashboard";
+const homePageRoute = "/home";
+const dashboardPageRoute = "/dashboard";
 const authenticationPageRoute = '/authentication';
-const   somiteeregistrationPageRoute = "/somiteeregistration";
-const   somiteelistPageRoute = "/somiteelist";
-const   memberegistrationPageRoute = "/memberregistration";
-const   memberlistPageRoute = "/memberlist";
-const   memberclosingPageRoute = "/memberclosing";
-const   closingmemberrequestPageRoute = "/closingmemberrequest";
-const   memberEditPageRoute = "/memberedit";
+const somiteeregistrationPageRoute = "/somiteeregistration";
+const somiteelistPageRoute = "/somiteelist";
+const memberegistrationPageRoute = "/memberregistration";
+const memberlistPageRoute = "/memberlist";
+const memberclosingPageRoute = "/memberclosing";
+const closingmemberrequestPageRoute = "/closingmemberrequest";
+const memberEditPageRoute = "/memberedit";
 
-
-const   sanctionloanrequestPageRoute = "/sanctionloanrequestPageRoute";
-const   sanctionloanapprovePageRoute = "/sanctionloanapprovePageRoute";
-const   sanctionloanapprovelistPageRoute = "/sanctionloanapprovelistPageRoute";
-const   sanctionloaneditPageRoute = "/sanctionloaneditlistPageRoute";
-const   loanrequestlistPageRoute = "/loanrequestlistPageRoute";
-const   loandisbursementPageRoute = "/loandisbursementPageRoute";
-const   repaymentrequestPageRoute = "/repaymentrequestPageRoute";
+const sanctionloanrequestPageRoute = "/sanctionloanrequestPageRoute";
+const sanctionloanapprovePageRoute = "/sanctionloanapprovePageRoute";
+const sanctionloanapprovelistPageRoute = "/sanctionloanapprovelistPageRoute";
+const sanctionloaneditPageRoute = "/sanctionloaneditlistPageRoute";
+const loanrequestlistPageRoute = "/loanrequestlistPageRoute";
+const loandisbursementPageRoute = "/loandisbursementPageRoute";
+const repaymentrequestPageRoute = "/repaymentrequestPageRoute";
