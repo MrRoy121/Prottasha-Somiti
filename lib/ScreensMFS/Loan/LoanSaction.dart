31846e8fda3f36a7460cd787651d0695f6a0d20f
MrRoy121 <nilashishroyjoy@gmail.com>
2024-03-09 00:00:40 +0600

    Fixed Loan Scheme Issue

--- lib/ScreensMFS/Loan/LoanSaction.dart
+++ lib/ScreensMFS/Loan/LoanSaction.dart
@@ -245,8 +245,9 @@ class _LoanSanctionState extends State<LoanSanction> {
         "Remarks": conremarks.text,
         "Service Amount": serviceamount,
         "Status": "Requested",
-        "Requested By": "${AuthService.to.user!.id}-(*)-${AuthService.to.user!.name}",
-        "Approved By":'',
+        "Requested By":
+            "${AuthService.to.user!.id}-(*)-${AuthService.to.user!.name}",
+        "Approved By": '',
         'ID': sanctionloanrequestid,
         "Grantor F Name": congrfname.text,
         'SL': numberOfItems + 1,
@@ -308,17 +309,18 @@ class _LoanSanctionState extends State<LoanSanction> {
 
     void _setuploanscheme(int ins) {
       double principleAmount = double.parse(consanctionlimit.text.toString());
-      double val = principleAmount/10000;
+      double val = principleAmount / 10000;
       setState(() {
         _selectedscheme = LoanSchemes[ins];
         _selectedinstalment = InstallmentFrequencyList[0];
         coninstallmentno.text = _selectedscheme.installmentno.toString();
         conduratioon.text = _selectedscheme.duration.toString();
         coninstallmentamount.text =
-            (_selectedscheme.installmentamount*val).toString();
-        conservicecharge.text = (_selectedscheme.servicecharge*val).toString();
+            (_selectedscheme.installmentamount * val).toString();
+        conservicecharge.text =
+            (_selectedscheme.servicecharge * val).toString();
         serviceamount = (double.parse(conservicecharge.text) +
-            double.parse(coninstallmentamount.text)*val);
+            double.parse(coninstallmentamount.text) * val);
       });
     }
 
